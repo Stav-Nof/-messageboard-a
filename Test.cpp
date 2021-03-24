523948f3d9adf4e52651c740317141a3623143c8
Ubuntu <ubuntu@ip-172-31-19-90.us-east-2.compute.internal>
2021-03-24 19:29:16 +0000

    test

--- Test.cpp
+++ Test.cpp
@@ -5,55 +5,40 @@
 using namespace ariel;
 using namespace std;
 
-TEST_CASE("Test empty board")
-{
-    Board board;
-    CHECK(board.read(/*row*/ 0, /*column*/ 0, Direction::Horizontal, 0) == string(""));
-    CHECK(board.read(/*row*/ 1, /*column*/ 1, Direction::Vertical, 0) == string(""));
-    CHECK(board.read(/*row*/ 2, /*column*/ 2, Direction::Horizontal, 2) == string("__"));
-    CHECK(board.read(/*row*/ 3, /*column*/ 3, Direction::Vertical, 3) == string("___"));
-}
-
-
-TEST_CASE("Test Vertical")
-{
-    Board board;
-    board.post(1, 1, Direction::Vertical, "Vertical");
-    CHECK(board.read(/*row*/ 1, /*column*/ 0, Direction::Horizontal, 2) == string("_V"));
-    CHECK(board.read(/*row*/ 2, /*column*/ 0, Direction::Horizontal, 2) == string("_e"));
-    CHECK(board.read(/*row*/ 3, /*column*/ 1, Direction::Horizontal, 2) == string("r_"));
-    CHECK(board.read(/*row*/ 4, /*column*/ 0, Direction::Horizontal, 3) == string("_t_"));
-    CHECK(board.read(/*row*/ 4, /*column*/ 1, Direction::Vertical, 2) == string("ti"));
-    CHECK(board.read(/*row*/ 4, /*column*/ 1, Direction::Vertical, 3) == string("tic"));
-    CHECK(board.read(/*row*/ 4, /*column*/ 1, Direction::Vertical, 4) == string("tica"));
-    CHECK(board.read(/*row*/ 4, /*column*/ 1, Direction::Vertical, 5) == string("tical"));
-}
-
-TEST_CASE("Test Horizontal")
-{
-    Board board;
-    board.post(1, 1, Direction::Horizontal, "Horizontal");
-    CHECK(board.read(/*row*/ 1, /*column*/ 0, Direction::Horizontal, 2) == string("_H"));
-    CHECK(board.read(/*row*/ 1, /*column*/ 0, Direction::Horizontal, 3) == string("_Ho"));
-    CHECK(board.read(/*row*/ 1, /*column*/ 1, Direction::Horizontal, 5) == string("Horiz"));
-    CHECK(board.read(/*row*/ 1, /*column*/ 5, Direction::Horizontal, 3) == string("zon"));
-    CHECK(board.read(/*row*/ 1, /*column*/ 1, Direction::Vertical, 1) == string("H"));
-    CHECK(board.read(/*row*/ 1, /*column*/ 1, Direction::Vertical, 2) == string("H_"));
-    CHECK(board.read(/*row*/ 1, /*column*/ 4, Direction::Vertical, 3) == string("_i_"));
-    CHECK(board.read(/*row*/ 0, /*column*/ 3, Direction::Vertical, 2) == string("_r"));
-}
-
-TEST_CASE("Test Horizontal and Vertical")
-{
-    Board board;
-    board.post(5, 5, Direction::Horizontal, "abcde");
-    board.post(3, 7, Direction::Vertical, "xyzw");
-    CHECK(board.read(/*row*/ 5, /*column*/ 6, Direction::Horizontal, 2) == string("bz"));
-    CHECK(board.read(/*row*/ 3, /*column*/ 6, Direction::Horizontal, 2) == string("_x"));
-    CHECK(board.read(/*row*/ 6, /*column*/ 6, Direction::Horizontal, 3) == string("_w_"));
-    CHECK(board.read(/*row*/ 5, /*column*/ 7, Direction::Horizontal, 4) == string("zde_"));
-    CHECK(board.read(/*row*/ 2, /*column*/ 7, Direction::Vertical, 6) == string("_xyzw_"));
-    CHECK(board.read(/*row*/ 5, /*column*/ 7, Direction::Vertical, 1) == string("z"));
-    CHECK(board.read(/*row*/ 4, /*column*/ 6, Direction::Vertical, 3) == string("_b_"));
-    CHECK(board.read(/*row*/ 5, /*column*/ 9, Direction::Vertical, 2) == string("e_"));
-}
+Board board;
+TEST_CASE("Test post and read"){
+    
+    
+    board.post(1, 1, Direction::Vertical, "kneset");
+    board.post(8, 2, Direction::Horizontal, "bibi");
+    board.post(8, 3, Direction::Vertical, "israel");
+    board.post(4, 4, Direction::Vertical, "hello");
+   
+    CHECK(board.read(/*row*/ 1, /*column*/ 1, Direction::Vertical, 2) == string("kn"));
+    CHECK(board.read(/*row*/ 2, /*column*/ 1, Direction::Vertical, 2) == string("ne"));
+    CHECK(board.read(/*row*/ 3, /*column*/ 1, Direction::Vertical, 3) == string("ese"));
+    CHECK(board.read(/*row*/ 4, /*column*/ 1, Direction::Vertical, 1) == string("s"));
+    CHECK(board.read(/*row*/ 5, /*column*/ 1, Direction::Vertical, 2) == string("et"));
+    CHECK(board.read(/*row*/ 6, /*column*/ 1, Direction::Vertical, 3) == string("t__"));
+    
+    CHECK(board.read(/*row*/ 8, /*column*/ 2, Direction::Horizontal, 2) == string("bi"));
+    CHECK(board.read(/*row*/ 8, /*column*/ 2, Direction::Horizontal, 4) == string("bibi"));
+    CHECK(board.read(/*row*/ 8, /*column*/ 3, Direction::Horizontal, 1) == string("i"));
+    CHECK(board.read(/*row*/ 8, /*column*/ 1, Direction::Horizontal, 3) == string("_bi"));
+    
+    CHECK(board.read(/*row*/ 8, /*column*/ 3, Direction::Vertical, 2) == string("is"));
+    CHECK(board.read(/*row*/ 8, /*column*/ 3, Direction::Vertical, 7) == string("israel_"));
+    CHECK(board.read(/*row*/ 7, /*column*/ 3, Direction::Vertical, 3) == string("_is"));
+    
+    CHECK(board.read(/*row*/ 4, /*column*/ 1, Direction::Horizontal, 4) == string("k__h"));
+    CHECK(board.read(/*row*/ 5, /*column*/ 1, Direction::Horizontal, 4) == string("e__e"));
+    CHECK(board.read(/*row*/ 6, /*column*/ 1, Direction::Horizontal, 4) == string("t__l"));
+    
+    CHECK(board.read(/*row*/ 4, /*column*/ 4, Direction::Vertical, 1) == string("h"));
+    CHECK(board.read(/*row*/ 5, /*column*/ 4, Direction::Vertical, 3) == string("ell"));
+    CHECK(board.read(/*row*/ 8, /*column*/ 4, Direction::Vertical, 2) == string("o_"));
+    
+    CHECK(board.read(/*row*/ 8, /*column*/ 1, Direction::Horizontal, 5) == string("_bioi"));
+    
+} 
+    
